b61d37eb1ee42219523cbcd55d9ac123b2971d8f
alexanderhodes <alexander.hodes@live.com>
2020-05-22 10:17:35 +0200

    optimized imports

--- src/users/resolvers/users.resolver.ts
+++ src/users/resolvers/users.resolver.ts
@@ -1,15 +1,11 @@
-import {Args, Parent, Query, ResolveField, Resolver} from "@nestjs/graphql";
+import {Args, Query, Resolver} from "@nestjs/graphql";
 import {User} from "../models/user.entity";
 import {UsersService} from "../services/users.service";
-import {ParkingSpaceService} from "../services/parking-space.service";
-import {ParkingSpace} from "../models/parking-space.entity";
-import {ParkingSpaceStatusService} from "../services/parking-space-status.service";
 
 @Resolver(of => User)
 export class UsersResolver {
 
-    constructor(private readonly usersService: UsersService,
-                private readonly parkingSpaceService: ParkingSpaceService) {}
+    constructor(private readonly usersService: UsersService) {}
 
     @Query(returns => [User])
     allUsers(): Promise<User[]> {
@@ -20,10 +16,5 @@ export class UsersResolver {
     findUserById(@Args('id') id: string): Promise<User> {
         return this.usersService.findOne(id);
     }
-
-    // @ResolveField()
-    // async parkingSpace(@Parent() user: User) {
-    //     return user.parkingSpace ? await this.parkingSpaceService.findOne(user.parkingSpace.id) : null;
-    // }
 
 }
